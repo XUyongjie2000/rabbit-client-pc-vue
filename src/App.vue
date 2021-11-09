01f5c037bd748ecbbacd555c8ea475ce0820a905
胥永杰 <99706807@qq.com>
2021-11-09 10:06:29 +0800

    update: change some files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <router-link to="/">测试121111</router-link>
+    <router-link to="/">测试12111112</router-link>
   </div>
   <router-view />
 </template>
