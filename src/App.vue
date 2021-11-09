05b4bde5a86bc670e203ea8be62239b6d83147f8
胥永杰 <99706807@qq.com>
2021-11-09 09:15:32 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <router-link to="/">测试</router-link>
+    <router-link to="/">测试12</router-link>
   </div>
   <router-view />
 </template>
